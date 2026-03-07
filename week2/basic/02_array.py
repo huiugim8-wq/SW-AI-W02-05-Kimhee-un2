e2b5a70863798107afef3b2a5efbff510a35eafe
huiugim8-wq <huiugim8@gmail.com>
2026-03-08 02:32:38 +0900

    second

--- week2/basic/02_array.py
+++ week2/basic/02_array.py
@@ -31,60 +31,71 @@
 - 새로운 배열을 만들어 값을 채워넣으세요
 """
 
+a=[
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
 def rotate_matrix_90(matrix):
-    """
-    2차원 배열을 시계방향으로 90도 회전
+   
+    N = len(matrix)
+    new_matrix = [[0 for _ in range(N)] for _ in range(N)]  
     
-    Args:
-        matrix: N x N 2차원 리스트
-    
-    Returns:
-        회전된 2차원 리스트
-    """
-    n = len(matrix)
-    
-    # TODO: n x n 크기의 새로운 배열을 생성하세요 (0으로 초기화)
-    pass
-        
-    # TODO: 원본 배열의 각 요소를 회전된 위치에 배치하세요
-    # 힌트: (i, j) 위치의 요소는 회전 후 (j, n-1-i) 위치로 이동
-    pass
-    
-    return rotated
+    for i in range(n):
+        for j in range(n):
+        new_matrix[j][n-1-i] = matrix[i][j]
 
-def print_matrix(matrix):
-    """배열을 보기 좋게 출력하는 헬퍼 함수"""
-    for row in matrix:
-        print(row)
+    return new_matrix
 
-# 테스트 케이스
-if __name__ == "__main__":
-    # 테스트 케이스 1: 3x3 배열
-    matrix1 = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
+#     # TODO: 원본 배열의 각 요소를 회전된 위치에 배치하세요
+#     # 힌트: (i, j) 위치의 요소는 회전 후 (j, n-1-i) 위치로 이동
+#     pass
     
-    print("원본 배열:")
-    print_matrix(matrix1)
-    print("\n회전 후:")
-    rotated1 = rotate_matrix_90(matrix1)
-    print_matrix(rotated1)
-    print()
-    
-    # 테스트 케이스 2: 4x4 배열
-    matrix2 = [
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10, 11, 12],
-        [13, 14, 15, 16]
-    ]
-    
-    print("원본 배열:")
-    print_matrix(matrix2)
-    print("\n회전 후:")
-    rotated2 = rotate_matrix_90(matrix2)
-    print_matrix(rotated2)
+#     return rotated
 
 
+# def print_matrix(matrix):
+#     """배열을 보기 좋게 출력하는 헬퍼 함수"""
+#     for row in matrix:
+#         print(row)
+
+# # 테스트 케이스
+# if __name__ == "__main__":
+#     # 테스트 케이스 1: 3x3 배열
+#     matrix1 = [
+#         [1, 2, 3],
+#         [4, 5, 6],
+#         [7, 8, 9]
+#     ]
+    
+#     print("원본 배열:")
+#     print_matrix(matrix1)
+#     print("\n회전 후:")
+#     rotated1 = rotate_matrix_90(matrix1)
+#     print_matrix(rotated1)
+#     print()
+    
+#     # 테스트 케이스 2: 4x4 배열
+#     matrix2 = [
+#         [1, 2, 3, 4],
+#         [5, 6, 7, 8],
+#         [9, 10, 11, 12],
+#         [13, 14, 15, 16]
+#     ]
+    
+#     print("원본 배열:")
+#     print_matrix(matrix2)
+#     print("\n회전 후:")
+#     rotated2 = rotate_matrix_90(matrix2)
+#     print_matrix(rotated2)
+
+
+
+# matrix = [
+#             [1, 2, 3],
+#             [4, 5, 6],
+#             [7, 8, 9]
+#         ]
+
+# rotate_matrix_90(matrix)
