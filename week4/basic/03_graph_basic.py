07c80a9514bef0659dfcee000bcd69118eee15d5
Heejun Kim <heejunkim@Heejunui-MacBookAir.local>
2026-03-20 23:18:18 +0900

    Implement graph creation logic

--- week4/basic/03_graph_basic.py
+++ week4/basic/03_graph_basic.py
@@ -41,12 +41,19 @@ def create_graph(vertices, edges, directed=False):
         그래프 딕셔너리
     """
     # TODO: 빈 그래프 초기화
-    pass
-    
+    graph = {}
+    # 그래프는 딕셔너리 초기화한다.
+    for i in range(vertices):
+         graph[i]=[]
     # TODO: 간선 추가
+    for u,v in edges:
+        if directed == False:
+            graph[u].append(v)
+            graph[v].append(u)
+        else:
+            graph[u].append(v)
     ## 간선 추가 (u에서 v로)
     ## 무방향 그래프면 반대 방향도 추가
-    pass
     
     return graph
 
