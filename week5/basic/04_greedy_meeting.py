c422a53fb6a553558e88427e542fe91e8468fc8d
Heejun Kim <heejunkim@Heejunui-MacBookAir.local>
2026-03-31 09:21:31 +0900

    Add week5 basic greedy meeting solution

--- week5/basic/04_greedy_meeting.py
+++ week5/basic/04_greedy_meeting.py
@@ -33,20 +33,21 @@ def select_meetings(meetings):
         (배정된 회의 개수, 선택된 회의 리스트)
     """
     # TODO: 회의가 없으면 0 반환
-    pass
-    
+    if not meetings:
+        return 0,[]
     # TODO: 종료 시간 기준으로 정렬
-    pass
+    
+    meetings.sort(key=lambda m: (m[1], m[0])) 
     
     selected = []
-    
-    # TODO: 첫 번째 회의 선택
-    pass
+    end_time = 0
     
     # TODO: 나머지 회의들 확인
     ## 이전 회의가 끝난 후 시작하는 회의만 선택
-    pass
-    
+    for start,end in meetings:
+        if start >= end_time:
+            selected.append((start,end))
+            end_time = end
     return len(selected), selected
 
 # 테스트 케이스
